4cf13a35aee88d35d90713f6d727c7ffcdb9e508
John Weir <john@smokinggun.com>
2015-10-01 10:18:45 -0700

    move example into its own file

---
+++ delay_example_test.go
@@ -0,0 +1,32 @@
+package delay
+
+import (
+	"bytes"
+	"fmt"
+	"time"
+)
+
+func ExampleDelayBuffer() {
+	var buf bytes.Buffer
+	pb := NewDelayBuffer(time.Second*1, &buf)
+
+	pb.Write([]byte("abc"))
+
+	b := make([]byte, 3)
+
+	s, _ := pb.Read(b)
+	fmt.Println(s)
+	fmt.Println(b)
+
+	time.Sleep(1 * time.Second)
+
+	s, _ = pb.Read(b)
+	fmt.Println(s)
+	fmt.Println(b)
+
+	// Output:
+	// 0
+	// [0 0 0]
+	// 3
+	// [97 98 99]
+}
--- delay_test.go
+++ delay_test.go
@@ -7,31 +7,6 @@ import (
 	"time"
 )
 
-func ExampleDelayBuffer() {
-	var buf bytes.Buffer
-	pb := NewDelayBuffer(time.Second*1, &buf)
-
-	pb.Write([]byte("abc"))
-
-	b := make([]byte, 4)
-
-	s, _ := pb.Read(b)
-	fmt.Println(s)
-	fmt.Println(b)
-
-	time.Sleep(1 * time.Second)
-
-	s, _ = pb.Read(b)
-	fmt.Println(s)
-	fmt.Println(b)
-
-	// Output:
-	// 0
-	// [0 0 0 0]
-	// 3
-	// [97 98 99 0]
-}
-
 func (pb *DelayBuffer) readExpects(b []byte, t *testing.T) {
 	o := make([]byte, len(b))
 	pb.Read(o)
